da76924443cba6a3404c0f6d5ce055b81d904293
CYGNOD <pramgunjeet@gmail.com>
2025-12-28 13:33:30 +0530

    Remove year display and change footer link
    Removed the current year display and updated the footer link.

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -9,13 +9,13 @@ interface Options {
 
 export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
-    const year = new Date().getFullYear()
+  //  const year = new Date().getFullYear()
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          <a href="https://cygnod.in">CygnodXPrankx</a>
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
